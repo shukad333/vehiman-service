3ef938718c6fd2a9d0cc401cfe0d3cd0678071a8
Shuhail Kadavath <skadavath@vmware.com>
2018-06-20 12:25:02 +0530

    Comm5

--- src/main/java/com/amoebiq/product/vehiman/model/Owner.java
+++ src/main/java/com/amoebiq/product/vehiman/model/Owner.java
@@ -15,6 +15,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 //Comm1
 //Comm3
+//Comm5
 @Entity
 @Table(name="owner")
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
